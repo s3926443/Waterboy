8c5d9c8be165bb5a3112b2096db3edaa02b9066a
Mohamed A. Ali <62313945+s3450966@users.noreply.github.com>
2022-04-29 00:59:44 +1000

    Solenoid valve

--- waterboy_mvf_all.ino
+++ waterboy_mvf_all.ino
@@ -332,3 +332,38 @@ void loop() {
   delay(1000); // Wait for 1000 millisecond(s)
   digitalWrite(LED_BUILTIN, LOW);
 }
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+const int relayEnable = 2;
+const int sensorPin = A0;
+const int thresholdMax = 800;
+const int thresholdMin = 10;
+void setup()
+{
+  pinMode(relayEnable, OUTPUT);
+  pinMode(sensorPin, INPUT);
+  Serial.begin(9600);
+  Serial.println("Soil Moisture Level: ");
+  delay(1000);
+}
+
+void loop()
+{
+  int sensorValue = analogRead(sensorPin);
+  sensorValue = map(sensorValue, thresholdMax, thresholdMin, 0, 100);
+  Serial.print("Moisture: ");
+  Serial.print(sensorValue);
+  Serial.println("%");
+
+  if (sensorValue < 0)
+  {
+    digitalWrite(relayEnable, LOW);
+    Serial.println("Relay ON");
+  }
+  else
+  {
+    digitalWrite(relayEnable, HIGH);
+    Serial.println("Relay OFF");
+  }
+  delay(1000);
+}
